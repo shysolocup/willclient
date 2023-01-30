89ab452fd604a9effe4a1640921afff9510c006c
nutmeg <88659700+TheFlameZEternal@users.noreply.github.com>
2023-01-30 08:36:05 -0600

    Create buttons.js

---
+++ examples/buttons.js
@@ -0,0 +1,34 @@
+const { Client } = require('discord.js');
+const discordClient = new Client({ /* your stuff here */ });
+
+const { PSClient } = require('discord.ps');
+const client = new PSClient({ client: discordClient, prefix: "." });
+
+const config = require('./config.json');
+
+
+client.event("ready", (ctx) => {
+    console.log(`Logged in as ${ctx.user.tag}`);
+});
+
+/* commands */
+
+client.command( {name: "buttons"}, async (ctx, cmd) => {
+    let trueButton = client.Button({
+        id: "true",
+        label: "True",
+        style: "primary"
+    });
+    
+    let falseButton = client.Button({
+        id: "false",
+        label: "False",
+        style: "Danger"
+    });
+    
+    let row = client.ActionRow([trueButton, falseButton]);
+    
+    client.reply("Choose wisely..", { components: [row]});
+});
+
+client.login(config.token);
