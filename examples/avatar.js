aad1f19a503f2bcda5acc4145a7194bea727f8ad
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-02-07 07:21:25 -0600

    Create avatar.js

---
+++ examples/avatar.js
@@ -0,0 +1,31 @@
+/* THIS RELIES ON DISCORD.PS VERSION 0.5 WHICH IS NOT CURRENTLY AVAILABLE */
+
+const { Client } = require('discord.js');
+const client = new Client({ /* your stuff here */ });
+
+const { PSClient } = require('discord.ps');
+const psc = new PSClient({ client: client, prefix: "!" });
+
+const config = require('./config.json');
+
+
+psc.event("ready", (ctx) => {
+    console.log(`Logged in as ${ctx.user.tag}`);
+});
+
+/* commands */
+
+psc.command( {name: "avatar", aliases: ["av"], cooldown: 5}, (ctx, cmd) => {
+    if (cmd.onCooldown) return psc.reply("Command is on cooldown!", {deleteAfter: 3});
+    
+    if (cmd.args[0]) {
+        let user = psc.fetchUser(cmd.args[0]);
+        return ctx.reply(psc.user.avatar(user));
+    }
+    else {
+        return ctx.reply(psc.user.avatar());
+    }
+});
+
+
+client.login(config.token);
