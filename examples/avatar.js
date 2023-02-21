d286c71857d24dfce66114ec2c05e5baba51b2cf
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-02-21 13:59:31 -0600

    Update avatar.js

--- examples/avatar.js
+++ examples/avatar.js
@@ -2,12 +2,10 @@
 
 const { Client } = require('discord.js');
 const client = new Client({ /* your stuff here */ });
-
 const { PSClient } = require('discord.ps');
 const psc = new PSClient({ client: client, prefix: "!" });
 
 const config = require('./config.json');
-
 
 psc.event("ready", (ctx) => {
     console.log(`Logged in as ${ctx.user.tag}`);
