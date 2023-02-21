4b1437ce6b60bf0580ea7378f352f88c20603aea
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-02-21 14:09:51 -0600

    Update cooldowns.js

--- examples/cooldowns.js
+++ examples/cooldowns.js
@@ -1,24 +1,22 @@
 const { Client } = require('discord.js');
-const discordClient = new Client({ /* your stuff here */ });
-
+const client = new Client({ /* your stuff here */ });
 const { PSClient } = require('discord.ps');
-const client = new PSClient({ client: discordClient, prefix: "." });
+const psc = new PSClient({ client: client, prefix: "." });
 
 const config = require('./config.json');
 
-
-client.event("ready", (ctx) => {
+psc.event("ready", (ctx) => {
     console.log(`Logged in as ${ctx.user.tag}`);
 });
 
 /* commands */
 
-client.command( {name: "ping", cooldown: 5}, async (ctx, cmd) => {
+psc.command( {name: "ping", cooldown: 5}, async (ctx, cmd) => {
     if (cmd.onCooldown) {
-        return client.reply(`Command is on cooldown for ${cmd.cooldown.time} seconds!`);
+        return ctx.reply(`Command is on cooldown for ${cmd.cooldown.time} seconds!`);
     }
     
-    client.reply("Ping! Command is not on cooldown!");
+    ctx.reply("Ping! Command is not on cooldown!");
 });
 
 client.login(config.token);
