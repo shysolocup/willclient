48e1e6f59e6efab2bdc3122a0a2b901843c84927
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-02-21 14:01:38 -0600

    Update basics.js

--- examples/basics.js
+++ examples/basics.js
@@ -1,20 +1,18 @@
 const { Client } = require('discord.js');
-const discordClient = new Client({ /* your stuff here */ });
-
+const client = new Client({ /* your stuff here */ });
 const { PSClient } = require('discord.ps');
-const client = new PSClient({ client: discordClient, prefix: "." });
+const psc = new PSClient({ client: client, prefix: "." });
 
 const config = require('./config.json');
 
-
-client.event("ready", (ctx) => {
+psc.event("ready", (ctx) => {
     console.log(`Logged in as ${ctx.user.tag}`);
 });
 
 /* commands */
 
-client.command( {name: "ping"}, async (ctx, cmd) => {
-    return client.reply("Pong!");
+psc.command( {name: "ping"}, async (ctx, cmd) => {
+    return ctx.reply("Pong!");
 });
 
 client.login(config.token);
