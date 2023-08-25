9914c311bf8d39b6cfa09c1f5995f52d13a8c91e
paige <88659700+paigeroid@users.noreply.github.com>
2023-08-25 15:26:08 -0500

    Update purge.js

--- examples/purge.js
+++ examples/purge.js
@@ -1,28 +1,28 @@
 const { Client } = require('discord.js');
 const client = new Client({ /* your stuff here */ });
-const { PSClient } = require('discordpps');
-const psc = new PSClient({ client: client, prefix: "!" });
+const { WillClient } = require('willclient');
+const wc = new WillClient({ client: client, prefix: "!" });
 
 const config = require('./config.json');
 
-psc.event("ready", (ctx) => {
+wc.event("ready", (ctx) => {
     console.log(`Logged in as ${ctx.user.tag}`);
 });
 
 /* commands */
 
-psc.command( {name: "purge", cooldown: 30}, async (ctx, cmd) => {
-    if (cmd.onCooldown) return psc.reply("Command is on cooldown.", {deleteAfter: 3});
+wc.command( {name: "purge", cooldown: 30}, async (ctx, cmd) => {
+    if (cmd.onCooldown) return wc.reply("Command is on cooldown.", {deleteAfter: 3});
     
     let amount = parseInt(cmd.args[0]);
     
-    if (psc.user.hasPermissions(["manageMessages"])) {
-        psc.channel.purge(amount);
+    if (wc.author.hasPermissions(["manageMessages"])) {
+        wc.channel.purge(amount);
         
-        await psc.channel.send(`Purged ${amount} messages.`, {deleteAfter: 3});
+        await wc.channel.send(`Purged ${amount} messages.`, {deleteAfter: 3});
     }
     else {
-        await psc.channel.send(`You don't have permission to run this command.`, {deleteAfter: 3});
+        await wc.channel.send(`You don't have permission to run this command.`, {deleteAfter: 3});
     }
 });
 
