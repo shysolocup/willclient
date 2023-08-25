276ec1b693a2fff57814f326fb3d51e4d78b4772
paige <88659700+paigeroid@users.noreply.github.com>
2023-08-25 12:28:36 -0500

    Update and rename permissionsList.txt to permissionsList.js

---
+++ lists/permissionsList.js
@@ -0,0 +1,265 @@
+permissionList = {
+
+
+	/* :: ADMINISTRATOR :: */
+		"administrator": "Administrator",
+		"admin": "Administrator",
+		
+		
+		
+	/* :: MESSAGES :: */
+	
+		// send messages
+		"sendMessages": "SendMessages",
+		"send": "SendMessages",
+		"message": "SendMessages",
+		
+		// send tts messages
+		"sendTTSMessages": "SendTTSMessages",
+		"sendTTS": "SendTTSMessages",
+		
+		// manage messages
+		"manageMessages": "ManageMessages",
+		
+		
+		
+	/* :: EMBEDS :: */
+	
+		// embed links
+		"embedLinks": "EmbedLinks",
+		"links": "EmbedLinks",
+		
+		// attach files
+		"attachFiles": "AttachFiles",
+		"sendFiles": "AttachFiles",
+		"files": "AttachFiles",
+		
+		
+	
+	/* :: MENTION :: */
+	
+		"mentionEveryone": "MentionEveryone",
+		"pingEveryone": "MentionEveryone",
+		"everyone": "MentionEveryone",
+		
+		
+		
+	/* :: NICKNAMES :: */
+	
+		// change nickname
+		"changeNickname": "ChangeNickname",
+		"changeNick": "ChangeNickname",
+		"nickname": "ChangeNickname",
+		"nick": "ChangeNickname",
+	
+		// manage nicknames
+		"manageNicknames": "ManageNickname",
+		"manageNickname": "ManageNickname",
+		"manageNicks": "ManageNickname",
+		"manageNick": "ManageNickname",
+		"nicknames": "ManageNickname",
+		"nicks": "ManageNickname",
+	
+	
+	
+	/* :: MEMBERS :: */
+	
+		// ban members
+		"banMembers": "BanMembers",
+		"banUsers": "BanMembers",
+		"ban": "BanMembers",
+		
+		// kick members
+		"kickMembers": "KickMembers",
+		"kickUsers": "KickMembers",
+		"kick": "KickMembers",
+	
+		// moderate members
+		"moderateMembers": "ModerateMembers",
+		"moderateUsers": "ModerateMembers",
+		"moderate": "ModerateMembers",
+		
+		
+	
+	/* :: CHANNELS :: */
+	
+		// manage channel
+		"manageChannels": "ManageChannels",
+		
+		// view channel
+		"viewChannel": "ViewChannel",
+		
+		
+	
+	/* :: ROLES :: */
+		
+		"manageRoles": "ManageRoles",
+		"roles": "ManageRoles",
+		
+		
+	
+	/* :: MESSAGE HISTORY :: */
+	
+		"readMessageHistory": "ReadMessageHistory",
+		"readHistory": "ReadMessageHistory",
+		"viewMessageHistory": "ReadMessageHistory",
+		"viewHistory": "ReadMessageHistory",
+	
+		
+	
+	/* :: GUILD :: */
+		
+		// manage guild
+		"manageGuild": "manageGuild",
+		
+		// guild insights
+		"viewGuildInsights": "ViewGuildInsights",
+		"guildInsights": "ViewGuildInsights",
+		"insights": "ViewGuildInsights",
+		
+		
+		
+		
+	/* :: REACTIONS :: */
+	
+		"addReactions": "AddReactions",
+		"reactions": "AddReactions",
+		"react": "AddReactions",
+		
+		
+	
+	/* :: EMOJIS & STICKERS :: */
+		
+		// use external emojis
+		"useExternalEmojis": "UseExternalEmojis",
+		"externalEmojis": "UseExternalEmojis",
+		"useEmojis": "UseExternalEmojis",
+		
+		// manage emojis and stickers
+		"manageEmojisAndStickers": "ManageEmojisAndStickers",
+		"emojisAndStickers": "ManageEmojisAndStickers",
+		
+		// manage emojis
+		"manageEmojis": "ManageEmojisAndStickers",
+		"emojis": "ManageEmojisAndStickers",
+		
+		// manage stickers
+		"manageStickers": "ManageEmojisAndStickers",
+		"stickers": "ManageEmojisAndStickers",
+	
+		
+		
+		
+	/* :: AUDIT LOGS :: */
+	
+		"viewAuditLog": "ViewAuditLogs",
+		"viewLogs": "ViewAuditLogs",
+		"auditLogs": "ViewAuditLogs",
+		"logs": "ViewAuditLogs",
+		
+	
+	
+	/* :: INVITES :: */
+		
+		"createInvite": "CreateInstantInvite",
+		
+		
+	
+	
+	/* :: VOICE :: */
+	
+		// connect
+		"connect": "Connect",
+		
+		// speak
+		"speak": "Speak",
+		
+		// stream
+		"stream": "Stream",
+		
+		// VAD
+		"VAD": "UseVAD",
+		"useVAD": "UseVAD",
+		
+		// move members
+		"move": "MoveMembers",
+		"moveMembers": "MoveMembers",
+		"moveUsers": "MoveMembers",
+	
+		// mute members
+		"mute": "MuteMembers",
+		"muteMembers": "MuteMembers",
+		"muteUsers": "MuteMembers",
+		
+		// deafean members
+		"deafen": "DeafenMembers",
+		"deafenMembers": "DeafenMembers",
+		"deafenUsers": "DeafenMembers",
+		
+		// priority speaker
+		"prioritySpeaker": "PrioritySpeaker",
+		"priority": "PrioritySpeaker",
+
+
+	
+	/* :: WEBHOOKS :: */
+	
+		"manageWebhooks": "ManageWebhooks",
+		
+		
+	/* :: Slash Commands :: */
+	
+		"useApplicationCommands": "UseApplicationCommands",
+		"useSlashCommands": "UseApplicationCommands",
+		"useCommands": "UseApplicationCommands",
+		"applicationCommands": "UseApplicationCommands",
+		"slashCommands": "UseApplicationCommands",
+		"commands": "UseApplicationCommands",
+	
+	
+	
+	/* :: EVENTS :: */
+	
+		"manageEvents": "ManageEvents",
+		"events": "ManageEvents",
+	
+	
+	
+	/* :: THREADS :: */
+	
+		// manage threads
+		"manageThreads": "ManageThreads",
+		"threads": "ManageThreads",
+	
+		// create public threads
+		"createPublicThreads": "CreatePublicThreads",
+		"publicThreads": "CreatePublicThreads",
+	
+		// create private threads
+		"createPrivateThreads": "CreatePrivateThreads",
+		"privateThreads": "CreatePrivateThreads",
+		
+		// send messages in threads
+		"sendMessagesInThreads": "SendMessagesInThreads",
+		"sendThreadMessages": "SendMessagesInThreads",
+		"threadMessages": "SendMessagesInThreads",
+		"messageInThreads": "SendMessagesInThreads",
+		
+		
+		
+	/* :: ACTIVITIES :: */
+	
+		"useEmbeddedActivities": "UseEmbeddedActivities",
+		"useActivities": "UseEmbeddedActivities",
+		"embeddedActivities": "UseEmbeddedActivities",
+		"activities": "UseEmbeddedActivities"
+		
+		
+	
+	/* :: STAGE :: */
+	
+		"requestToSpeak": "RequestToSpeak",
+
+};
+
+module.exports = { permissionList }
