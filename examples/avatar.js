f20b48de6d3d7b1e92c51d5a99158fd6528a1cfc
paige <88659700+paigeroid@users.noreply.github.com>
2023-08-25 15:18:46 -0500

    Update avatar.js

--- examples/avatar.js
+++ examples/avatar.js
@@ -1,25 +1,25 @@
 const { Client } = require('discord.js');
 const client = new Client({ /* your stuff here */ });
-const { PSClient } = require('discordpps');
-const psc = new PSClient({ client: client, prefix: "!" });
+const { WillClient } = require('willclient');
+const wc = new WillClient({ client: client, prefix: "!" });
 
 const config = require('./config.json');
 
-psc.event("ready", (ctx) => {
+wc.event("ready", (ctx) => {
     console.log(`Logged in as ${ctx.user.tag}`);
 });
 
 /* commands */
 
-psc.command( {name: "avatar", aliases: ["av"], cooldown: 5}, (ctx, cmd) => {
-    if (cmd.onCooldown) return psc.reply("Command is on cooldown!", {deleteAfter: 3});
+wc.command( {name: "avatar", aliases: ["av"], cooldown: 5}, (ctx, cmd) => {
+    if (cmd.onCooldown) return wc.reply("Command is on cooldown!", {deleteAfter: 3});
     
     if (cmd.args[0]) {
-        let user = psc.fetchUser(cmd.args[0]);
-        return ctx.reply(psc.user.avatar(user));
+        let user = wc.fetchUser(cmd.args[0]);
+        return ctx.reply(wc.user.avatar(user));
     }
     else {
-        return ctx.reply(psc.user.avatar());
+        return ctx.reply(wc.user.avatar());
     }
 });
 
