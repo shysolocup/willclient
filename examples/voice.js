310ab4760626190b91b8569c8fea3e2f918d1d90
paige <88659700+paigeroid@users.noreply.github.com>
2023-08-29 14:52:15 -0500

    Update voice.js

--- examples/voice.js
+++ examples/voice.js
@@ -1,9 +1,9 @@
 const { Client } = require('discord.js');
 const client = new Client({ /* your stuff here */ });
 const { WillClient } = require('willclient');
-const wc = new PSClient({ client: client, prefix: "." });
-
 const config = require('./config.json');
+
+const wc = new WillClient({ client: client, prefix: "!", token: config.token });
 
 wc.event("ready", (ctx) => {
     console.log(`Logged in as ${ctx.user.tag}`);
