efd47c73e7f056525b0711aa6c5b6ad09d127007
paige <88659700+paigeroid@users.noreply.github.com>
2023-08-30 12:33:29 -0500

    Create slash commands.js

---
+++ examples/slash commands.js
@@ -0,0 +1,19 @@
+const { Client } = require('discord.js');
+const client = new Client({ /* your stuff here */ });
+const { WillClient } = require('willclient');
+const config = require('./config.json');
+
+const wc = new WillClient({ client: client, prefix: "!", token: config.token });
+
+wc.event("ready", (ctx) => {
+    console.log(`Logged in as ${ctx.user.tag}`);
+});
+
+/* commands */
+
+wc.slashCommand("ping", "Replies with pong!", async (ctx, cmd) => {
+    ctx.reply("Pong!");
+});
+
+
+client.login(config.token);
