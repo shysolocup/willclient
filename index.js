472d86d7b1a2ee5bf86d7d5225206719221242d5
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-10 17:17:26 -0500

    Added TypeScript support to building and compiling

--- index.js
+++ index.js
@@ -131,18 +131,18 @@ class WillClient {
 	}
 
 
-	build(path, ignore=["index.js"], action=(path, file) => { require(`../../${path}/${file}`); }) {
-		let files = fs.readdirSync(`${path}`).filter(file => (file.endsWith('.js') && !ignore.includes(file) ));
+	build(path, ignore=["index.js", "index.ts"], action=(path, file) => { require(`../../${path}/${file}`); }) {
+		let files = fs.readdirSync(`${path}`).filter(file => ((file.endsWith('.js') || file.endsWith('.ts')) && !ignore.includes(file) ));
 		files.forEach( (file) => { action(path, file) } );
 	}
 
 
 	compile(path, ignore=["index.js"], action=(path, file, compiled, name) => { compiled.push(name, require(`../../${path}/${file}`)); }, json=true) {
-		let files = fs.readdirSync(`${path}`).filter(file => ((file.endsWith('.js') || (json && file.endsWith(".json"))) && !ignore.includes(file) ));
+		let files = fs.readdirSync(`${path}`).filter(file => ((file.endsWith('.js') || file.endsWith('.ts') || (json && file.endsWith(".json"))) && !ignore.includes(file) ));
 		let stuff = new Soup(Object);
 		
 		files.forEach( (file) => {
-			let name = (file.endsWith(".js")) ? file.split(".js")[0] : (file.endsWith(".json")) ? file.split(".json")[0] : file;
+			let name = (file.endsWith(".js")) ? file.split(".js")[0] : (file.endsWith('.ts')) ? file.split('.ts')[0] : (file.endsWith(".json")) ? file.split(".json")[0] : file;
 			action(path, file, stuff, name);
 		});
 
